c55cbabb1aa359ab8565d1623f3b4a7f398704c8
akii1998 <110331686+akii1998@users.noreply.github.com>
2022-12-20 12:04:58 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import './App.css';
+// import React from "react";
+// import {  Routes, Route, BrowserRouter } from "react-router-dom";
+
+// import "./App.css";
+// import Home from "./pages/Home";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//     <Routes>
+//       <div className="App">
+//         <Route path='/' element={<Home/>} />
+//       </div>
+//     </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import {  createBrowserRouter ,RouterProvider } from "react-router-dom";
+
+import "./App.css";
+import Home from "./pages/Home";
+import NetflixShowPage from "./pages/NetflixShowPage";
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/netflix-show',
+    element:<NetflixShowPage/>
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <div className="App">
+        <RouterProvider router={router} />
+      </div>
   );
 }
 
---
+++ src/pages/NetflixShowPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NetflixShowPage = () => {
+  return (
+    <div>
+        <h1>Netflix Page</h1>
+    </div>
+  )
+}
+
+export default NetflixShowPage
