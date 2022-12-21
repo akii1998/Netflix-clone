74ff2f7c36b4b51e7470ec7183f3ccdc4dc2b96c
akii1998 <110331686+akii1998@users.noreply.github.com>
2022-12-22 02:35:45 +0530

    package

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,11 +1,21 @@
-import React, {useEffect,useState} from 'react'
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
 
-const Row = ({title, fetchUrl, isLargeRow}) => {
+
+const base_url = 'https://image.tmdb.org/t/p/original';
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response =await axios.get(`${process.env.REACT_APP_API}${fetchUrl}`)
+    };
+  });
   return (
     <div>
-        <h2></h2>
+      <h2></h2>
     </div>
-  )
-}
+  );
+};
 
-export default Row
+export default Row;
